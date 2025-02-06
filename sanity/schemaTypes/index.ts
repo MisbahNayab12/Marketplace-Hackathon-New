f5fa61f2e628eb0d82408a9c4faa5fa06fce2507
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2025-02-06 20:16:12 +0500

    another marketplace repository

--- sanity/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity';
-import chef from './chefs';
-import food from './foods';
-
-import orders from './orders';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [food, chef, orders],
-};
