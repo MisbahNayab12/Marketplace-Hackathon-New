32784c2a4564754423043fff1ea48393483dbc7d
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2025-02-06 20:06:29 +0500

    updated

--- app/Products/[id]/page.tsx
+++ app/Products/[id]/page.tsx
@@ -5,18 +5,27 @@ interface ProductPageProps {
   params: { id: string };
 }
 
-// ✅ Server Component for fetching product data
+export async function generateStaticParams() {
+  const products = await client.fetch(`*[_type == "food"]{ _id }`);
+  return products.map((product: { _id: string }) => ({ id: product._id }));
+}
+
 export default async function ProductPage({ params }: ProductPageProps) {
+
+  console.log("Params received in ProductPage:", params)
+
   const product = await getProduct(params.id);
 
-  if (!product) return <p>Product not found</p>;
+  if (!product) 
+    {
+      return <p>Product not found</p>
+    };
 
   return (
-    <ProductDetail product={product} /> // ✅ Pass product data to Client Component
+    <ProductDetail product={product} />
   );
 }
 
-// ✅ Fetch data on the server
 async function getProduct(id: string) {
   return client.fetch(
     `*[_type == "food" && _id == $id][0]{
